37f5ccab5e987b82501c2be65dff7e061e779102
overflowlife <kno1232012.g@gmail.com>
2021-02-12 11:07:08 +0900

    delete powered

--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php
 echo (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-echo "\n<p><address>#written and powered by twitter<a href='https://twitter.com/@overflowlife'>@overflowlife</a>.</address></p>";
