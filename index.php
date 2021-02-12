a381f03403bee227c43d0ddb451b545879ed3cd5
overflowlife <kno1232012.g@gmail.com>
2021-02-12 10:02:04 +0900

    modify powered by arake

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 echo (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-echo "\n\n#written and powered by twitter<a href='https://twitter.com/@overflowlife'>@overflowlife</a>.";
+echo "\n<p><address>#written and powered by twitter<a href='https://twitter.com/@overflowlife'>@overflowlife</a>.</address></p>";
